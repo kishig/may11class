b28b592e4f8491854fdb6840f33badcbc9fba3e5
kishig <84652819+kishig@users.noreply.github.com>
2021-05-24 00:47:02 -0500

    Add files via upload

---
+++ ifelse5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cout << "Please enter some number " << endl;
+	cin >> num;
+	
+	if (num % 2 == 0)
+	{
+		cout << "The number you have entered is even number" << endl;
+		
+	}
+	else
+	{
+		cout << "The number you have entered is odd number" << endl;
+	}
+	
+		
+	
+	return 0;
+}
