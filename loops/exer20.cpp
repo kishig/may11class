cf362f1d23506e181280d190d40b66c7d89a699b
kishig <84652819+kishig@users.noreply.github.com>
2021-06-02 14:05:22 -0500

    Add files via upload

---
+++ loops/exer20.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	char askii;
+	
+
+	
+	for (int i = 0; i <= 255; i++)
+	{
+		askii = i;
+		cout << "ASCII value of character " << askii << " = " << i << endl;	
+	}
+	
+	
+	return 0;
+}	
+		
+	
